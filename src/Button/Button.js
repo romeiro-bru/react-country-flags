43810fd2a06a19f465591821b0fa025393d0d155
Bruna Romeiro <56081906+romeiro-bru@users.noreply.github.com>
2021-04-26 15:30:46 -0300

    Add mo flags

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -2,9 +2,12 @@ import React from "react";
 import { useState } from "react";
 import "./style.css";
 
+import china from "./china.svg";
 import india from "./india.svg";
 import aust from "./australia.svg";
 import brazil from "./brazil.svg";
+import germany from "./germany.svg";
+import france from "./france.svg";
 
 export function Button() {
   const [message, SetMessage] = useState("Show some country flags");
@@ -16,9 +19,12 @@ export function Button() {
 
   const flags = (
     <section hidden={message === "Show some country flags"} className="flags">
+      <img src={china} alt="china" />
       <img src={india} alt="india" />
       <img src={aust} alt="australia" />
       <img src={brazil} alt="brazil" />
+      <img src={germany} alt="germany" />
+      <img src={france} alt="france" />
     </section>
   );
 
