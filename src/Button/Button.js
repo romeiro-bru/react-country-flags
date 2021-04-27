906ff5d20cb66b904a021bbe6f9bb371341cc87f
Bruna Romeiro <56081906+romeiro-bru@users.noreply.github.com>
2021-04-26 22:01:31 -0300

    Refactor flag component. Create new variables to msgs

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,37 +1,22 @@
 import React from "react";
 import { useState } from "react";
+import { Flag } from "../Flag/Flag";
+
 import "./style.css";
 
-import china from "./china.svg";
-import india from "./india.svg";
-import aust from "./australia.svg";
-import brazil from "./brazil.svg";
-import germany from "./germany.svg";
-import france from "./france.svg";
-
 export function Button() {
-  const [message, SetMessage] = useState("Show some country flags");
-  const handleClick = () => {
-    message === "Show some country flags"
-      ? SetMessage("Flags are boring")
-      : SetMessage("Show some country flags");
-  };
+  const oldVariable = "Show some country flags";
+  const newVariable = "Flags are boring";
 
-  const flags = (
-    <section hidden={message === "Show some country flags"} className="flags">
-      <img src={china} alt="china" />
-      <img src={india} alt="india" />
-      <img src={aust} alt="australia" />
-      <img src={brazil} alt="brazil" />
-      <img src={germany} alt="germany" />
-      <img src={france} alt="france" />
-    </section>
-  );
+  const [message, SetMessage] = useState(oldVariable);
+  const handleClick = () => {
+    message === oldVariable ? SetMessage(newVariable) : SetMessage(oldVariable);
+  };
 
   return (
     <div>
       <button onClick={handleClick}>{message}</button>
-      {flags}
+      <Flag hide={message === oldVariable} />
     </div>
   );
 }
